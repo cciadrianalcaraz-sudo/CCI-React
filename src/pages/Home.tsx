7711d6c2a96cf2b381b7c4c01470a349969923c5
Adrian Alcaraz Preciado <a.alcarazpreciado@gmail.com>
2026-02-11 10:59:26 -0600

    cambio de secciones

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,8 +9,8 @@ export default function Home() {
     return (
         <main>
             <Hero />
-            <About />
             <Services />
+            <About />
             <ValueProposition />
             <Sectors />
             <Contact />
